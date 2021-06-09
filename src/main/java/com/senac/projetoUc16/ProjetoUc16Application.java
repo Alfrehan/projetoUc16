0d6286f3eedb2f67e84d3d02dd5cc9d6882375e5
Alfrehan <a.hannzel@gmail.com>
2021-06-08 21:14:50 -0300

    Initial commit

---
+++ src/main/java/com/senac/projetoUc16/ProjetoUc16Application.java
@@ -0,0 +1,13 @@
+package com.senac.projetoUc16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoUc16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoUc16Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/senac/projetoUc16/ProjetoUc16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.projetoUc16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoUc16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
